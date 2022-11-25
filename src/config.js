1907de1b08dd3f55bb060be8a1533d6c3890f29a
Alejandro Trikitrakatelas Sanchez Cano <99841453+AlejandroTRIKITRAKATELAS33sanchezCano@users.noreply.github.com>
2022-11-24 18:34:08 -0600

    2

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ export const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET ; //
 
 export const PAYPAL_API = process.env.PAYPAL_API ;
 
-export const PORT = process.env.PORT || 3000 ; //Nose si se tenga que cambiar el puerto dependiendo de como indique el lugar donde se hoste
+export const PORT = process.env.PORT || 3000   ; //Nose si se tenga que cambiar el puerto dependiendo de como indique el lugar donde se hoste
 
 export const HOST = process.env.HOST ; //Creo que se tendría que cambiar por el link de railway o heroku
 
